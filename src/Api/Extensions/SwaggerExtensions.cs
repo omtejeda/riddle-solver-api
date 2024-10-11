03a77c066883eac51584b9139b8efcadcff56923
omtejeda <omarky12-@hotmail.com>
2024-10-10 20:35:43 -0400

    docs: add XML comments for SwaggerExtensions

--- src/Api/Extensions/SwaggerExtensions.cs
+++ src/Api/Extensions/SwaggerExtensions.cs
@@ -4,8 +4,16 @@ using RiddleSolver.Api.Constants;
 
 namespace RiddleSolver.Api.Extensions;
 
+/// <summary>
+/// Extension methods for setting up Swagger in the API.
+/// </summary>
 public static class SwaggerExtensions
 {
+    /// <summary>
+    /// Adds Swagger services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection to add Swagger to.</param>
+    /// <returns>The updated service collection.</returns>
     public static IServiceCollection AddApiSwagger(this IServiceCollection services)
     {
         services.AddSwaggerGen(c =>
@@ -24,6 +32,11 @@ public static class SwaggerExtensions
         return services;
     }
 
+    /// <summary>
+    /// Configures the Swagger UI in the specified <see cref="IApplicationBuilder"/>.
+    /// </summary>
+    /// <param name="app">The application builder to configure Swagger UI for.</param>
+    /// <returns>The updated application builder.</returns>
     public static IApplicationBuilder UseApiSwagger(this IApplicationBuilder app)
     {
         var provider = app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
