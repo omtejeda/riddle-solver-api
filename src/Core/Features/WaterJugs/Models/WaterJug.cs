ac1e2fe523b5d151bdea7a02cfbaa4630961ea26
omtejeda <omarky12-@hotmail.com>
2024-10-10 20:30:26 -0400

    docs: add XML comments for params in WaterJug record

--- src/Core/Features/WaterJugs/Models/WaterJug.cs
+++ src/Core/Features/WaterJugs/Models/WaterJug.cs
@@ -3,4 +3,7 @@ namespace RiddleSolver.Core.Features.WaterJugs.Models;
 /// <summary>
 /// Represents the configuration for the Water Jug problem, including the capacities of the jugs and the desired amount of water.
 /// </summary>
+/// <param name="CapacityX">The capacity of the first jug.</param>
+/// <param name="CapacityY">The capacity of the second jug.</param>
+/// <param name="AmountWanted">The desired amount of water to measure.</param>
 public record struct WaterJug(int CapacityX, int CapacityY, int AmountWanted);
