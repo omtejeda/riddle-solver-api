f06e71862f232c726f477e0c4bfa8f5a098419f4
omtejeda <omarky12-@hotmail.com>
2024-10-10 20:33:17 -0400

    docs: add XML comments for ResultExtensions

--- src/Api/Extensions/ResultExtensions.cs
+++ src/Api/Extensions/ResultExtensions.cs
@@ -2,17 +2,27 @@ using Microsoft.AspNetCore.Mvc;
 using RiddleSolver.Core.Models;
 using RiddleSolver.Core.Enums;
 
-namespace RiddleSolver.Api.Extensions;
-
-public static class ResultExtensions
+namespace RiddleSolver.Api.Extensions
 {
-    public static IActionResult ToActionResult<T>(this Result<T> result) where T : class
+    /// <summary>
+    /// Extension methods for <see cref="Result{T}"/>.
+    /// </summary>
+    public static class ResultExtensions
     {
-        return result.Success switch
+        /// <summary>
+        /// Converts a <see cref="Result{T}"/> to an <see cref="IActionResult"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the result data.</typeparam>
+        /// <param name="result">The result to convert.</param>
+        /// <returns>An <see cref="IActionResult"/> based on the result succes and/or status.</returns>
+        public static IActionResult ToActionResult<T>(this Result<T> result) where T : class
         {
-            true => new OkObjectResult(result),
-            _ when result.Status.Equals(Status.Unprocessable.ToString()) => new UnprocessableEntityObjectResult(result),
-            _ => new BadRequestObjectResult(result)
-        };
+            return result.Success switch
+            {
+                true => new OkObjectResult(result),
+                _ when result.Status.Equals(Status.Unprocessable.ToString()) => new UnprocessableEntityObjectResult(result),
+                _ => new BadRequestObjectResult(result)
+            };
+        }
     }
 }
